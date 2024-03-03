21b36039364137015522ed06942faf6052f68584
Nicholas Djedjos <72480037+reformist@users.noreply.github.com>
2024-03-02 23:08:53 -0500

    parse URL info

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,12 +1,28 @@
 // content.js
 console.log("LeetCode Helper content script loaded!");
 
-// Access the prompt or other information on the page
-const promptElement = document.querySelector('.question-content');
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+  let url = tabs[0].url;
+  // use `url` here inside the callback because it's asynchronous!
+  // console.log(url);
+  if (url.includes("leetcode.com/problems/")) {
+    console.log("this is the correct url");
+    url_parts = url.split('/');
+    
+    // console.log(url_parts) the problem is url_parts[4]
 
-if (promptElement) {
-  const promptText = promptElement.textContent.trim();
-  console.log("LeetCode prompt:", promptText);
+    problem = url_parts[4];
+    console.log(problem);  
+  }
+});
 
-  // Now, you can manipulate or use the promptText as needed.
-}
+
+// // Access the prompt or other information on the page
+// const promptElement = document.querySelector('.question-content');
+
+// if (promptElement) {
+//   const promptText = promptElement.textContent.trim();
+//   console.log("LeetCode prompt:", promptText);
+
+//   // Now, you can manipulate or use the promptText as needed.
+// }
