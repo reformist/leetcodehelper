399d4502bc0d97f3ea578e47e2e84415f724a376
Nicholas_Djedjos <ndjedjos@gmail.com>
2024-04-10 16:14:08 -0400

    nothing

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,6 +1,18 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      files: ['content.js']
-    });
-  });
+// chrome.action.onClicked.addListener((tab) => {
+//     chrome.scripting.executeScript({
+//       target: {tabId: tab.id},
+//       files: ['content.js']
+//     });
+//   });
+
+chrome.runtime.onInstalled.addListener(function() {
+  // Register the onClicked event listener
+  chrome.browserAction.onClicked.addListener(buttonClicked);
+});
+function buttonClicked() {
+  console.log("button clicked!");
+}
+  
+
+console.log("background is running")
+
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -10,7 +10,10 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
     const url_parts = url.split('/');
     const problem = url_parts[4];
     console.log(problem);
+    console.log(tabs);
 
+
+    extractCodeFromViewLines();
     /** 
     // Inject code to extract text from view-line elements
     chrome.scripting.executeScript({
@@ -18,20 +21,26 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
       function: extractCodeFromViewLines,
     });
 
+    
+
     */
 
-    extractCodeFromViewLines();
   }
 });
 
+
+
+console.log("gets out of leetcode problem");
 // The function to extract code lines
 function extractCodeFromViewLines() {
-  const codeLines = document.querySelectorAll('.view-line');
+  console.log("goes into extractcode");
+  const codeLines = document.querySelector('.view-line');
   let code = '';
 
   // Iterate through each line except the last one
   for (let i = 0; i < codeLines.length - 1; i++) {
-    const text = codeLines[i].textContent.replace(/\u00a0/g, ' '); // Replace non-breaking spaces with regular spaces
+    const text = codeLines[i].textContent.replace(/\u00a0/g, ' ');
+    console.log(text); // Replace non-breaking spaces with regular spaces
     code += text + '\n'; // Add a newline after each line
   }
 
