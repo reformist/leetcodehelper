86d3414975445d60014433ac66ee98b41624e863
paeb37 <btp2109@columbia.edu>
2024-04-21 10:59:39 -0400

    for deployment

--- chrome_extension/old_code.js
+++
@@ -1,7 +0,0 @@
-    // Inject code to extract text from view-line elements
-    /**
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      function: extractCodeFromViewLines,
-    });
-    */
